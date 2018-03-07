9d0f2f014bda040bcfedaa96a1a2a30a5fdf36e9
Dan Rosen <drosen84@gmail.com>
2018-03-07 16:17:56 -0600

    created get endpoint test

--- test/test-recipes.js
+++ test/test-recipes.js
@@ -1,69 +1,32 @@
 'use strict';
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-
-const {app, runServer, closeServer} = require('../server');
-
-// this lets us use *expect* style syntax in our tests
-// so we can do things like `expect(1 + 1).to.equal(2);`
-// http://chaijs.com/api/bdd/
-const expect = chai.expect;
-
-// This let's us make HTTP requests
-// in our tests.
-// see: https://github.com/chaijs/chai-http
-chai.use(chaiHttp);
-
-
 describe('Recipes', function() {
 
-  // Before our tests run, we activate the server. Our `runServer`
-  // function returns a promise, and we return the that promise by
-  // doing `return runServer`. If we didn't return a promise here,
-  // there's a possibility of a race condition where our tests start
-  // running before our server has started.
   before(function() {
     return runServer();
   });
-
-  // although we only have one test module at the moment, we'll
-  // close our server at the end of these tests. Otherwise,
-  // if we add another test module that also has a `before` block
-  // that starts our server, it will cause an error because the
-  // server would still be running from the previous tests.
   after(function() {
     return closeServer();
   });
-
-  // test strategy:
-  //   1. make request to `/shopping-list`
-  //   2. inspect response object and prove has right code and have
-  //   right keys in response object.
   it('should list items on GET', function() {
-    // for Mocha tests, when we're dealing with asynchronous operations,
-    // we must either return a Promise object or else call a `done` callback
-    // at the end of the test. The `chai.request(server).get...` call is asynchronous
-    // and returns a Promise, so we just return it.
     return chai.request(app)
-      .get('/shopping-list')
+      .get('/recipes')
       .then(function(res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
         expect(res.body).to.be.a('array');
-
-        // because we create three items on app load
+  
         expect(res.body.length).to.be.at.least(1);
         // each item should be an object with key/value pairs
         // for `id`, `name` and `checked`.
-        const expectedKeys = ['id', 'name', 'checked'];
+        const expectedKeys = ['id', 'name', 'ingredients'];
         res.body.forEach(function(item) {
           expect(item).to.be.a('object');
           expect(item).to.include.keys(expectedKeys);
         });
       });
   });
-
+  
   // test strategy:
   //  1. make a POST request with data for a new item
   //  2. inspect response object and prove it has right
@@ -84,7 +47,7 @@ describe('Recipes', function() {
         expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
       });
   });
-
+  
   // test strategy:
   //  1. initialize some update data (we won't have an `id` yet)
   //  2. make a GET request so we can get an item to update
@@ -101,9 +64,9 @@ describe('Recipes', function() {
       name: 'foo',
       checked: true
     };
-
+  
     return chai.request(app)
-      // first have to get so we have an idea of object to update
+    // first have to get so we have an idea of object to update
       .get('/shopping-list')
       .then(function(res) {
         updateData.id = res.body[0].id;
@@ -116,8 +79,8 @@ describe('Recipes', function() {
           .put(`/shopping-list/${updateData.id}`)
           .send(updateData);
       })
-      // prove that the PUT request has right status code
-      // and returns updated item
+    // prove that the PUT request has right status code
+    // and returns updated item
       .then(function(res) {
         expect(res).to.have.status(200);
         expect(res).to.be.json;
@@ -125,15 +88,15 @@ describe('Recipes', function() {
         expect(res.body).to.deep.equal(updateData);
       });
   });
-
+  
   // test strategy:
   //  1. GET shopping list items so we can get ID of one
   //  to delete.
   //  2. DELETE an item and ensure we get back a status 204
   it('should delete items on DELETE', function() {
     return chai.request(app)
-      // first have to get so we have an `id` of item
-      // to delete
+    // first have to get so we have an `id` of item
+    // to delete
       .get('/shopping-list')
       .then(function(res) {
         return chai.request(app)
@@ -144,3 +107,4 @@ describe('Recipes', function() {
       });
   });
 });
+  
